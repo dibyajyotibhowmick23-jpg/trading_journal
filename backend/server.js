e022b6a9d2158511eaab3e8b6476cc844ccfe92c
Dibyajyoti <dibyajyotibhowmick23@gmail.com>
2026-04-23 11:57:06 +0530

    mongo fix final

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ app.use(cors());
 app.use(express.json());
 
 /* ---------------- CONNECT MONGODB ---------------- */
-mongoose.connect("mongodb://127.0.0.1:27017/tradingApp");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => {
+  console.log("✅ MongoDB Connected");
+})
+.catch((err) => {
+  console.log("❌ MongoDB Error:", err);
+});
 
 /* ---------------- MODEL ---------------- */
 const TradeSchema = new mongoose.Schema({
